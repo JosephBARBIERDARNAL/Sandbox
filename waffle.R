a953281e82549b14f55949892c1a33999dbd0a8c
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-05-29 12:34:15 +0200

    autopush

--- waffle.R
+++ waffle.R
@@ -1,6 +1,17 @@
-library(viridis)
-library(ggplot2)
-dsub <- subset(diamonds, x > 5 & x < 6 & y > 5 & y < 6)
-dsub$diff <- with(dsub, sqrt(abs(x - y)) * sign(x - y))
-d <- ggplot(dsub, aes(x, y, colour = diff)) + geom_point()
-d + scale_color_viridis() + theme_bw()
+library(ggraph)
+library(igraph)
+library(tidyverse)
+library(dplyr)
+library(dendextend)
+library(colormap)
+
+data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/13_AdjacencyUndirecterWeighted.csv", header=T, row.names="Cities.", sep=",")# %>% as.matrix
+colnames(data) <- gsub("\\.", " ", colnames(data))
+data <- as.data.frame(data)
+str(data)
+str(data)
+head(data)
+
+dend <- as.dist(data) %>%
+  hclust(method="ward.D") %>%
+  as.dendrogram()
